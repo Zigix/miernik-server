46b6ccc2c4059b203d3c28f606ba2871f2d14456
Zigix <olekwszk7@gmail.com>
2024-06-10 09:49:36 +0200

    Initial commit

---
+++ src/main/java/com/miernikserver/MiernikServerApplication.java
@@ -0,0 +1,13 @@
+package com.miernikserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiernikServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiernikServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miernikserver/MiernikServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miernikserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiernikServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
